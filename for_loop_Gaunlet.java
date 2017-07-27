df240a46ece8681df8a2dbe263c1d78c467cb929
NanoGamer21 <nanogamer21@gmail.com>
2017-07-27 14:25:40 -0700

    Add files via upload

---
+++ for_loop_Gaunlet.java
@@ -0,0 +1,19 @@
+
+public class for_loop_Gaunlet {
+public static void main(String[] args) {
+	for (int i = 2003; i < 2018; i++) {
+		//System.out.println(i);
+	
+	System.out.println("I am" +" " +(i-2003)+" " + "in " + i);
+	
+           
+	}
+	
+	}
+
+private static void println(String string) {
+	// TODO Auto-generated method stub
+	
+}
+}
+
